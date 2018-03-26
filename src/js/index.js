fd3bf13f8c8cde4a59bf9db5adc23a3fccc5aa7b
Giacomo7 <giacomocardella@hotmail.com>
2018-03-26 18:13:01 +0200

    print.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
 import _ from 'lodash';
-import './style.css';
+import '../scss/style.css';
+import printMe from './print.js'
 
 function component() {
   var element = document.createElement('div');
+  //var button = createElement('button');
 
+  //button.innerHTML = 'Click me and check the console';
+  //button.onclick = printMe;
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
